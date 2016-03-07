74661dced9fbbb49e6a9659fce06c64e0c34cf78
Drew Schuster <dschuster@nerdwallet.com>
2016-03-07 10:44:53 -0800

    Filters now can be selects

--- components/filters.jsx
+++ components/filters.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateFilter } from '../redux/actions.js'
+import { status as statusOptions, priority as priorityOptions } from '../bug-enums'
+
+const selectFilters = {
+  priority: priorityOptions,
+  status: statusOptions
+}
+
+const renderSelect = (dispatch, filterKey, filterValue) => {
+  var enums = [...selectFilters[filterKey].enum].reverse()
+  enums.unshift(undefined)
+  return (
+    <span key={filterKey} className='filter'>
+      <select value={filterValue} onChange={(e) => {
+        var value = e.target.value === filterKey ? undefined : e.target.value
+        dispatch(updateFilter({[filterKey]: value}))
+      }}>
+        {enums.map((option) => {
+          return (<option key={option} value={option}>{option || filterKey}</option>)
+        })}
+      </select>
+    </span>
+  )
+}
 
 const Filters = ({filters, dispatch}) => {
   var filterNames = Object.keys(filters)
@@ -9,6 +32,9 @@ const Filters = ({filters, dispatch}) => {
       <h3>Filters</h3>
       {
         filterNames.map((filter) => {
+          if (selectFilters[filter]) {
+            return renderSelect(dispatch, filter, filters[filter])
+          }
           return (
             <span key={filter} className='filter'>
               <input type='text' placeholder={filter} value={filters[filter]} onChange={(e) => {
