8e5c463a604f77a3146017858576e38f4432af65
Drew Schuster <dschuster@nerdwallet.com>
2016-03-24 15:34:28 -0700

    foo

--- index.jsx
+++ index.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import classnames from 'classnames'
+
 require('./styles/index.less')
 
+var Foo = React.createClass({
+  propTypes: {
+    bugTrackerURL: React.PropTypes.string,
+    newBugEndpoint: React.PropTypes.string.isRequired,
+    projectName: React.PropTypes.string.isRequired,
+    reinitStore: React.PropTypes.func
+  },
+  getDefaultProps: function () {
+    return {
+      bugTrackerURL: '//drewschuster-0.nerdwallet.biz',
+      reInitStore: function () {},
+      newBugEndpoint: '/api/bugs'
+    }
+  },
+  render: function () {
+    debugger
+    return <span/>
+  }
+})
 var Index = React.createClass({
   displayName: 'Index',
 
   render: function () {
     return (
-      <div className={classnames('index')}>Hello World</div>
+      <div className={classnames('index')}>Hello World
+        <Foo projectName='drew'/>
+      </div>
     )
   }
 })
+
 
 ReactDOM.render(<Index/>, document.getElementById('root'))
